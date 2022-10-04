11284ab0f0a4be218b9c50cd140013dea789f880
Mphathi Maapola <113903905+Patglu@users.noreply.github.com>
2022-10-04 17:56:55 +0200

    Initial Commit

---
+++ 3DEcommerceApp/_DEcommerceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  _DEcommerceAppApp.swift
+//  3DEcommerceApp
+//
+//  Created by Pat on 2022/10/04.
+//
+
+import SwiftUI
+
+@main
+struct _DEcommerceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
